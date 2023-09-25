d32d7b0284ac03ef9a04aecb6c63effa006376da
Nasiullha Chaudhari <94208165+N4si@users.noreply.github.com>
2023-09-25 10:56:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,6 @@ from flask import Flask
 app = Flask(__name__)
 @app.route('/')
 def index():
-    return '<h1 style="color: blue;">New Application deployed through Argonaut</h1><p>Version 2</p>'
+    return '<h1 style="color: blue;">New Application deployed through Argonaut</h1><p>Version 1</p>'
 if __name__ == '__main__':
   app.run()
